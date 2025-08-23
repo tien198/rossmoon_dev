ee8a6bc765229544072940a4f36ff1c7ea407931
tien998 <tienvn998@gmai.com>
2025-08-23 09:37:45 +0700

    update UI

--- src/components/layouts/Navbar/Nav.Footer.tsx
+++ src/components/layouts/Navbar/Nav.Footer.tsx
@@ -3,12 +3,12 @@ import styles from './nav.module.scss'
 export default function Footer() {
     return (
         <div
-            className={
-                styles['nav__list--services']
-            }
+            className={styles['nav__list--services']}
         >
-            <p>Chúng tôi có thể giúp gì cho bạn?</p>
-            <p className="font-medium">+84 2838614107</p>
+            <div className={styles['service']}>
+                <p>Chúng tôi có thể giúp gì cho bạn?</p>
+                <p className="font-medium">+84 2838614107</p>
+            </div>
         </div>
     )
 }
